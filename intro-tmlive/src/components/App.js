df0dcc6eb08928a2155736a8e0e7e5ddcc7b2cd4
Sean Ruhland <sean.ruhland@gmail.com>
2018-04-16 23:43:55 -0700

    router working

--- intro-tmlive/src/components/App.js
+++ intro-tmlive/src/components/App.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import Home from './Home'
 import About from './About'
 import Topics from './Topics'
+import {
+  BrowserRouter,
+  Route,
+  Link,
+} from 'react-router-dom'
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
-        <About />
-        <Home />
-        <Topics />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <ul>
+            <li><Link to='/' >Home</Link></li>
+            <li><Link to='/about' >About</Link></li>
+            <li><Link to='/topics' >Topics</Link></li>
+
+
+            <hr />
+
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/topics' component={Topics}/>
+
+          </ul>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- intro-tmlive/src/components/Topics.js
+++ intro-tmlive/src/components/Topics.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import {
+  Link,
+  Route,
+} from 'react-router-dom'
 
-export default function Topic () {
-  return <div>TOPICS</div>
+
+function Topic ({match}) {
+  return <h3>{match.params.topicId}</h3>
+}
+export default function Topics ({match}) {
+  return (
+    <div>
+      <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${match.url}/rendering` }>Rendering with React</Link>
+          </li>
+           <li>
+            <Link to={`${match.url}/components` }>Components</Link>
+          </li>
+           <li>
+            <Link to={`${match.url}/props-v-state` } >Props vs State</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route path='/topics/:topicId' component={Topic} ></Route>
+
+    </div>
+  )
 }
